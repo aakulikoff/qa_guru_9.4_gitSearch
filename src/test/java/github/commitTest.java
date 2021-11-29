344bdef53d238de22ffaa485ce8d1d509c33747a
Andrey Kulikov <9230697@gmail.com>
2021-11-29 10:20:22 +0300

    добавил первый шаг

--- src/test/java/github/commitTest.java
+++ src/test/java/github/commitTest.java
@@ -2,10 +2,12 @@ package github;
 
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.open;
+
 public class commitTest {
 
     @Test
     void commitTest(){
-
+        open("https://github.com/");
     }
 }
